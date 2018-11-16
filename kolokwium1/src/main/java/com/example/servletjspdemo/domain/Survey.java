e0d0e996235f4f0fef07f2db3bc17bb34505f369
Artur Pyśk <apysk@INF.UG.EDU.PL>
2018-11-16 12:21:02 +0100

    added kolokwium1

---
+++ kolokwium1/src/main/java/com/example/servletjspdemo/domain/Survey.java
@@ -0,0 +1,54 @@
+package com.example.servletjspdemo.domain;
+
+public class Survey {
+	
+    private String dateFrom;
+    private String dateTo;
+    private String frequency;
+    private String comment;
+    
+    public Survey() {
+    	super();
+    }
+    
+    public Survey(String dateFrom, String dateTo, String frequency, String comment) {
+    	this.dateFrom = dateFrom;
+    	this.dateTo = dateTo;
+    	this.frequency = frequency;
+    	this.comment = comment;
+    }
+
+	public String getDateFrom() {
+		return dateFrom;
+	}
+
+	public void setDateFrom(String dateFrom) {
+		this.dateFrom = dateFrom;
+	}
+
+	public String getDateTo() {
+		return dateTo;
+	}
+
+	public void setDateTo(String dateTo) {
+		this.dateTo = dateTo;
+	}
+
+	public String getFrequency() {
+		return frequency;
+	}
+
+	public void setFrequency(String frequency) {
+		this.frequency = frequency;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+    
+
+}
---
+++ kolokwium1/src/main/java/com/example/servletjspdemo/web/DataAddCartServlet.java
@@ -0,0 +1,55 @@
+package com.example.servletjspdemo.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.example.servletjspdemo.domain.Guitar;
+import com.example.servletjspdemo.service.StorageService;
+
+@WebServlet(urlPatterns = "/data-cart")
+public class DataAddCartServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		StorageService ss = (StorageService) getServletContext().getAttribute("storage_service");
+		
+		List<Guitar> availableGuitars = ss.getAllGuitars();
+		Guitar guitarToCart = availableGuitars.get(Integer.parseInt(request.getParameter("add")) - 1);
+
+		HttpSession ses = request.getSession();
+		if (ses.getAttribute("cart") == null) {
+			List<Guitar> cart = new ArrayList<Guitar>();
+			ses.setAttribute("cart", cart);
+		}
+		
+		@SuppressWarnings("unchecked")
+		List<Guitar> cart = (List<Guitar>) ses.getAttribute("cart");
+		cart.add(guitarToCart);
+		response.sendRedirect("cart");
+		out.close();
+	}
+	
+	@Override
+	public void init() throws ServletException {
+
+		// application context
+		getServletContext().setAttribute("storage_service", new StorageService());
+	}
+}
